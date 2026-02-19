524a58ddc31f62ec39bfbfc136e79718b0b7e307
veerranchore <veerranchordaspagi@gmail.com>
2026-02-19 19:35:04 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout.tsx';
-import HomePage from './pages/HomePage.tsx';
-import ServicesPage from './pages/ServicesPage.tsx';
-import AboutPage from './pages/AboutPage.tsx';
-import CareersPage from './pages/CareersPage.tsx';
-import ContactPage from './pages/ContactPage.tsx';
-import ScrollToTop from './components/ScrollToTop.tsx';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
+import ServicesPage from './pages/ServicesPage';
+import AboutPage from './pages/AboutPage';
+import CareersPage from './pages/CareersPage';
+import ContactPage from './pages/ContactPage';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
